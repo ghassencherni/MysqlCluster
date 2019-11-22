b96c39fc452c5532d0b1081ca4f331c87005abf9
ghassen charni <ghassen.cherni@gmail.com>
2019-11-22 18:24:25 +0000

    modified:   MysqlCluster/files/getfilepos.sh 	modified:   MysqlCluster/tasks/init.yml 	new file:   MysqlCluster/templates/mysql.sql.j2 	modified:   MysqlCluster/templates/setslave.sh.j2 	new file:   MysqlCluster/templates/setslave.sh.j2_back

--- MysqlCluster/files/getfilepos.sh
+++ MysqlCluster/files/getfilepos.sh
@@ -1,4 +1,4 @@
 mas_log_file="`cat /master_status.txt | awk '{print $1}'`"
 mas_log_pos="`cat /master_status.txt | awk '{print $2}'`"
-echo -e "MASTER_LOG_FILE=$mas_log_file \nMASTER_LOG_POS=$mas_log_pos" > /tmp/share/mysql.env
-chmod 777 /tmp/share/mysql.env
+echo -e "#!/bin/bash \nexport MASTER_LOG_FILE=$mas_log_file \nexport MASTER_LOG_POS=$mas_log_pos" > /tmp/share/mysql.sh
+chmod 777 /tmp/share/mysql.sh
